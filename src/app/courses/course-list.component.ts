42663bf503cc4506b2bde25a2988c418ca8db6bd
Augusto Fernandes Costa <augusto.fcosta1808@gmail.com>
2022-05-27 22:15:53 -0300

    O que é injeção de dependência

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { Course } from "./course";
+import { CourseService } from "./course.service";
 
 @Component({
     selector:'app-course-list',
@@ -7,29 +8,9 @@ import { Course } from "./course";
 })
 export class CourseListComponent implements OnInit{
     courses: Course[] = [];
-
+    constructor(private courseService: CourseService){}
     ngOnInit(): void {
-        this.courses = [
-            {
-                id: 1,
-                name: 'Angular: Forms',
-                imageUrl: '/assets/images/forms.png',
-                price: 99.99,
-                code: 'xps-8796',
-                duration: 120,
-                rating: 4.5,
-                realeaseDate: 'December, 2 , 2022'
-            },
-            {
-                id: 1,
-                name: 'Angular: HTTP',
-                imageUrl: '/assets/images/http.png',
-                price: 45.99,
-                code: 'LKL-1094',
-                duration: 80,
-                rating: 4,
-                realeaseDate: 'December, 4 , 2022'
+        this.courses = this.courseService.retrieveAll();
+       
             }
-        ]
-    }
 }
